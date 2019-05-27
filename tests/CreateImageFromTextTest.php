20ac44210c5d5082b6c35e1ef980c6909d682cff
Andrej <alahucky@protonmail.com>
2019-05-27 16:30:52 +0300

    Initial commit

---
+++ tests/CreateImageFromTextTest.php
@@ -0,0 +1,24 @@
+<?php
+	declare(strict_types=1);
+
+	namespace Hracik\CreateImageFromText\Tests;
+
+	use PHPUnit\Framework\TestCase;
+
+	final class CreateImageFromTextTest extends TestCase
+	{
+
+		/**
+		 * @dataProvider createImageFromTextProvider
+		 */
+		public function testGetIntegerFromString()
+		{
+
+		}
+
+
+		public function createImageFromTextProvider()
+		{
+
+		}
+	}
